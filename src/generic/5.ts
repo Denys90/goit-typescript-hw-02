92ed8e1785d982ebadbf7c6cf6447355d6e744a2
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-02-02 18:08:51 +0100

    generic

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, N> {
+  key: K;
+  value: N;
 }
 
 export {};
