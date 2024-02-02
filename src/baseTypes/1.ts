fc047e3d39588d626bbde24ea77f48224d1f5e0d
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-02-02 21:39:00 +0100

    fixed

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -5,8 +5,8 @@ let name: string = "Max";
 let toggle: boolean = true;
 let empty: null = null;
 let notInitialize: any;
-let callback = (a: number) => {
+let callback = (a: number): number => {
   return 100 + a;
 };
 
-export {};
+export { callback };
